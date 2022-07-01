038d9df08bddcaffd1111ae4c4e9d00c346160f2
tomek-zawadzki <65092372+tomek-zawadzki@users.noreply.github.com>
2022-07-01 11:40:24 +0200

    download information about my repozitories from github API and add it to html

--- src/js/index.js
+++ src/js/index.js
@@ -7,3 +7,27 @@ import '../scss/main.scss';
 /* place your code below */
 
 console.log('HELLO 🚀')
+
+fetch("https://api.github.com/users/tomek-zawadzki/repos?sort=created&direction=asc")
+ .then(resp => resp.json())
+ .then(resp => {
+for (let repo of resp) {
+    const {name, description, html_url, url} = repo;
+    const reposData = document.querySelector('.window--js');
+    const myTemplate =  `<div class="window window--js">
+    <div class="window__dots">
+    <img class="window__dots--element" src="/src/assets/img/circles_top.svg">
+    </div>
+    <img class="window__img" alt="Github logo" src="/src/assets/img/Vector.png">
+    <ul class="window__list">
+      <li class="window__list--element">project:${name}</li>
+      <li class="window__list--element">description:${description}</li>
+      <li class="window__list--element">demo:${url}</li>
+      <li class="window__list--element">github:${html_url}</li>
+    </ul>`;
+    reposData.innerHTML += myTemplate;
+}
+})
+.catch(error =>{
+    console.log(error);
+})
