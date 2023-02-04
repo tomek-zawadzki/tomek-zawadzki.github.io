3941a5150739914987a4db9295e1ad4036dbb7ed
tomek-zawadzki <65092372+tomek-zawadzki@users.noreply.github.com>
2023-02-04 14:30:49 +0100

    add navigate by href to sections, and same style fixes

--- src/js/index.js
+++ src/js/index.js
@@ -13,9 +13,10 @@ fetch(
 )
   .then((resp) => resp.json())
   .then((resp) => {
+    console.log(resp);
     const projectsSection = document.querySelector(".window__container");
     for (let repo of resp) {
-      const { name, description, html_url, url } = repo;
+      const { name, description, html_url, homepage } = repo;
       const myTemplate = `<div class="window window--js">
     <div class="window__dots">
     <img class="window__dots--element" src="/src/assets/img/circles_top.svg">
@@ -24,7 +25,7 @@ fetch(
     <ul class="window__list">
       <li class="window__list--element"><p class ="window__list--p" >project:</p> <span class="window__list--name">${name}</span></li>
       <li class="window__list--element window__list--element-specjal "><p class ="window__list--p">description:</p> <span class="window__list--description"> ${description} </span></li>
-      <li class="window__list--element"><p class ="window__list--p">demo:</p> <a class="window__list--url" href="${url}"> &lt;see here&gt;</a> </li>
+      <li class="window__list--element"><p class ="window__list--p">demo:</p> <a class="window__list--url" href="${homepage}"> &lt;see here&gt;</a> </li>
       <li class="window__list--element"><p class ="window__list--p">github:</p> <a class="window__list--url" href="${html_url}">&lt;source code&gt;</a></li>
     </ul>`;
 
