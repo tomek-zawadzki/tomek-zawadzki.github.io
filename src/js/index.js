e79d5dc51d29bd8d57fb98129c5a2cf9819c3217
tomek-zawadzki <65092372+tomek-zawadzki@users.noreply.github.com>
2023-02-04 13:29:40 +0100

    styling projects section for desktop and small fixes in mobile

--- src/js/index.js
+++ src/js/index.js
@@ -8,28 +8,30 @@ import "../scss/main.scss";
 
 console.log("HELLO 🚀");
 
-// fetch(
-//   "https://api.github.com/users/tomek-zawadzki/repos?sort=created&direction=asc"
-// )
-//   .then((resp) => resp.json())
-//   .then((resp) => {
-//     for (let repo of resp) {
-//       const { name, description, html_url, url } = repo;
-//       const reposData = document.querySelector(".window--js");
-//       const myTemplate = `<div class="window window--js">
-//     <div class="window__dots">
-//     <img class="window__dots--element" src="/src/assets/img/circles_top.svg">
-//     </div>
-//     <img class="window__img" alt="Github logo" src="/src/assets/img/Vector.png">
-//     <ul class="window__list">
-//       <li class="window__list--element">project:${name}</li>
-//       <li class="window__list--element">description:${description}</li>
-//       <li class="window__list--element">demo:${url}</li>
-//       <li class="window__list--element">github:${html_url}</li>
-//     </ul>`;
-//       reposData.innerHTML += myTemplate;
-//     }
-//   })
-//   .catch((error) => {
-//     console.log(error);
-//   });
+fetch(
+  "https://api.github.com/users/tomek-zawadzki/repos?sort=created&direction=asc"
+)
+  .then((resp) => resp.json())
+  .then((resp) => {
+    const projectsSection = document.querySelector(".window__container");
+    for (let repo of resp) {
+      const { name, description, html_url, url } = repo;
+      const myTemplate = `<div class="window window--js">
+    <div class="window__dots">
+    <img class="window__dots--element" src="/src/assets/img/circles_top.svg">
+    </div>
+    <img class="window__img" alt="Github logo" src="/src/assets/img/Vector.png">
+    <ul class="window__list">
+      <li class="window__list--element"><p class ="window__list--p" >project:</p> <span class="window__list--name">${name}</span></li>
+      <li class="window__list--element window__list--element-specjal "><p class ="window__list--p">description:</p> <span class="window__list--description"> ${description} </span></li>
+      <li class="window__list--element"><p class ="window__list--p">demo:</p> <a class="window__list--url" href="${url}"> &lt;see here&gt;</a> </li>
+      <li class="window__list--element"><p class ="window__list--p">github:</p> <a class="window__list--url" href="${html_url}">&lt;source code&gt;</a></li>
+    </ul>`;
+
+      //   reposData.innerHTML += myTemplate;
+      projectsSection.insertAdjacentHTML("beforeend", myTemplate);
+    }
+  })
+  .catch((error) => {
+    console.log(error);
+  });
